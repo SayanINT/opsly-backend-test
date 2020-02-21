c113975a34954e3459862b603a21e47926ebf9ed
SayanMannaDev <sayan.manna@indusnet.co.in>
2020-02-21 16:59:58 +0530

    initial commit..

---
+++ src/main/kotlin/com/example/opslytest/OpslytestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.opslytest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpslytestApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpslytestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/opslytest/OpslytestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.opslytest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpslytestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
